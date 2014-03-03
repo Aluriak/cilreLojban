5f3479b281e1f55d279f26127394344ccb25e199
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2014-03-04 00:12:32 +0100

    I want it to run !

--- src/updater/updater.py
+++ src/updater/updater.py
@@ -14,14 +14,35 @@ import re
 import tempfile
 from urllib.request import urlopen
 
-from sourceparser import Parser
+from updater.sourceparser import Parser
+from settings.settings import DB_NAME, SOURCE_DICT
 
-from settings import DBNAME, SOURCE_DICT
+def crossref_building(dbh):
+    """ Create crossrefs """
 
-if __name__ == '__main__':
+    cf_regex = re.compile('\(cf\. ([^\)]+)\)')
+    gismu_regex = re.compile(' (\w{5})')
+
+    c = dbh.cursor()
+    all_gismu = c.execute("SELECT rowid,gismu,comment FROM dico;").fetchall()
+
+    # then create a dict of dict :
+    gismu_list = {_[1]:{'id':_[0], 'comment':_[2]} for _ in all_gismu}
+
+    for gismu,dic in gismu_list:
+        print('Processing {}'.format(gismu))
+        referenced = gismu_regex.findall(cf_regex.search(dic['comment']).groups()[0])
+
+        map(
+            lambda g: c.execute('INSERT INTO crossref(src,ref) VALUES(?,?)', (dic['id'], gismu_list.get(g)['id'])),
+            referenced
+        )
+
+
+def main():
 
     # first, clean Db and regenerate it
-    db = slqite3.connect_db(DBNAME)
+    db = sqlite3.connect(DB_NAME)
     db.execute("DROP TABLE dico     IF EXISTS;")
     db.execute("DROP TABLE crossref IF EXISTS;")
     with open('schema.sql') as f:
@@ -41,7 +62,12 @@ if __name__ == '__main__':
 
         p = Parser(f)
         c.executemany(
-            'INSERT INTO dico(gismu,rafsi,english,globalenglish,definition,unknown,comment) values (?,?,?,?,?,?,?);', p)
+            'INSERT INTO dico(gismu,rafsi,english,globalenglish,definition,unknown,comment) VALUES (?,?,?,?,?,?,?);', p)
 
     c.close()
+
+    crossref_building(db)
+
     db.close()
+
+if __name__=='__main__': main()
