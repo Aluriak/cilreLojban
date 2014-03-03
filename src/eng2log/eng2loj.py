9fc2e83e5d9214f2e035ab0b1cc5ef64429b54e5
Aluriak <lucas.bourneuf@laposte.net>
2014-03-03 23:40:56 +0100

    draft for english to lojban

---
+++ src/eng2log/eng2loj.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#########################
+#       ENG2LOJ         #
+#########################
+
+
+#########################
+# IMPORTS               #
+#########################
+import sys
+from gismu import Gismu 
+
+
+
+#########################
+# PRE-DECLARATIONS      #
+#########################
+
+
+
+
+
+
+
+#########################
+# TREATMENT             #
+#########################
+arg = sys.argv
+print(arg)
+
+
+gismu = Gismu(english=argv[1])
+if gismu.founded:
+    print(gismu)
+else:
+    print("No gismu founded !")
+
+
+
