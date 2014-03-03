dfbf6e09460cb769ba97d9d8edeff29f57804ee6
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2014-03-03 23:07:20 +0100

    Updater code in itself

--- src/updater/updater.py
+++ src/updater/updater.py
@@ -7,84 +7,40 @@
 #########################
 # IMPORTS               #
 #########################
+import sys
+
 import sqlite3
-import parse
+import tempfile
+from urllib2 import urlopen
 
+from sourceparser import Parser
 
-
-#########################
-# PRE-DECLARATIONS      #
-#########################
-SOURCE_DICT = "http://www.lojban.org/publications/wordlists/gismu.txt"
-DB_NAME = "data/db"
-LOCAL_DICT_NAME = "data/dict"
-
-
-
-
-#########################
-# FUNCTIONS             #
-#########################
-def creatDB(dbDescriptor):
-        """Destroy existing DB, and creat nex ones, empty and ready to use.
-        Wait for a DB descriptor opened with sqlite3.connect() function"""
-        db.execute("DROP TABLE dico     IF EXISTS;")
-        db.execute("DROP TABLE crossref IF EXISTS;")
-        #TODO: creat DB
-        return None
-
-
-def getDataFile(source=SOURCE_DICT, filename=LOCAL_DICT_NAME):
-        """Search data file on the web, at SOURCE_DICT by default, and save it in LOCAL_DICT_NAME"""
-        #TODO
-        return None
-
-
-def dataRow2dict(line):
-        """
-        Return a string as "gismu varchar(8), rafsi varchar(32), english varchar(32), etc"
-        values can be used in a INSERT SQL query, and readed in line. 
-        line must be format as this example : 
- gismu [rafsi] english [english2] definition number1 number2 precision; reference
-        """
-        parser = parse.compile("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{};\t{}")
-        print(parser.parse(line))
-        return "TODO"
-
-
-def getRefFor(line):
-        """
-        Return gismu referenced in given line ("cf..." tag)
-        """
-        #TODO
-        return "TODO"
-
-
-
+from settings import DBNAME, SOURCE_DICT
 
 if __name__ == '__main__':
-        # INIT
-        #getDataFile()
-        #db = sqlite3.connect(DB_NAME)
-        #creatDB(db)
 
-        ## TREATMENT
-        #cursor = db.cursor()
-        #try:
-                #f = open(LOCAL_DICT_NAME, "r")
-                #for line in f.readlines():
-                        #sqline = dataRow2dict(line)
-                        #c.execute("INSERT INTO dico VALUES(" + sqline + ");")
+    # first, clean Db and regenerate it
+    db = slqite3.connect_db(DBNAME)
+    db.execute("DROP TABLE dico     IF EXISTS;")
+    db.execute("DROP TABLE crossref IF EXISTS;")
+    with open('schema.sql') as f:
+        db.execute(f.read())
 
-                        
-        #except:
-                #print("FILE ERROR: " + LOCAL_DICT_NAME)
+    c = db.cursor()
 
-        
-        ## END
-        #cursor.close()
-        #db.close()
+    # then grab the file
+    try:
+        distant_dict = urllopen(SOURCE_DICT)
+    except URLError as err:
+        sys.stderr.write("Well... urllopen just didn't work: {}".format(err))
 
+    with tempfile.TemporaryFile() as f:
+        print('Writing distant dic content to tempfile....')
+        f.write(distant_dict.read())
 
+        p = Parser(f)
+        c.executemany(
+            'INSERT INTO dico(gismu,rafsi,english,globalenglish,definition,unknown,comment) values (?,?,?,?,?,?,?);', p)
 
-
+    c.close()
+    db.close()
